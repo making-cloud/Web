03d32a9fa91757e181f0a041ef109ebaf11e6cb6
Eomhyunjun <zzp1318@gmail.com>
2022-10-13 08:41:28 +0900

    feat: 네비게이션 추가

---
+++ making_cloud/src/component/Main/Main.js
@@ -0,0 +1,11 @@
+import PageWrapper from "../Wrapper/PageWrapper";
+
+function Main(props) {
+  return (
+    <PageWrapper>
+        메인페이지입니다.
+    </PageWrapper>
+  );
+}
+
+export default Main;
---
+++ making_cloud/src/component/My/My.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PageWrapper from "../Wrapper/PageWrapper";
+
+function My(props) {
+  return <PageWrapper>마이페이지입니다.</PageWrapper>;
+}
+
+export default My;
