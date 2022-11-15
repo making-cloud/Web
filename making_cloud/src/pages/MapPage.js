6d2f472e5c055481e94b30c6c6bcb34fdd0ddabe
kmg <gkgkalsrms@naver.com>
2022-11-15 14:34:24 +0900

    지도 추가하기 마커 표시하기

--- making_cloud/src/pages/MapPage.js
+++ making_cloud/src/pages/MapPage.js
@@ -6,52 +6,47 @@ import MapContainer from "./kakaoMaps";
 
 
 const MapPage=()=>{
-    
-    
+  
     const [lists1,setLists]=useState([]);
-    const [lists2,setLists2]=useState([]);
     
-    
-  useEffect(()=>{
+  
+  useEffect(()=>{   //api 불러오기
       axios
       .all(
-        [axios.get('https://api.odcloud.kr/api/15073796/v1/uddi:17fbd06c-45bb-48aa-9be7-b26dbc708c9c?page=1&perPage=72&serviceKey=%2FQWCPnc%2F3l84xQe0ii7xaN5QUwH5zCG1J0a9UgUeHfLVzPq2HEQ0GBnTIqfUvdpJnqpg8CoWe37h%2F2%2F7pAJHbQ%3D%3D'),
-        axios.get('https://api.odcloud.kr/api/15060926/v1/uddi:2fbc5375-a15d-4907-9482-ecc12da41af2?page=1&perPage=10&serviceKey=%2FQWCPnc%2F3l84xQe0ii7xaN5QUwH5zCG1J0a9UgUeHfLVzPq2HEQ0GBnTIqfUvdpJnqpg8CoWe37h%2F2%2F7pAJHbQ%3D%3D'),])
-      .then(axios.spread((response1,response2)=>{
+        [axios.get('https://api.odcloud.kr/api/15073796/v1/uddi:17fbd06c-45bb-48aa-9be7-b26dbc708c9c?page=1&perPage=72&serviceKey=%2FQWCPnc%2F3l84xQe0ii7xaN5QUwH5zCG1J0a9UgUeHfLVzPq2HEQ0GBnTIqfUvdpJnqpg8CoWe37h%2F2%2F7pAJHbQ%3D%3D')])
+      .then(axios.spread((response1)=>{
         setLists(response1.data.data);
-        setLists2(response2.data.data);
-        console.log(response1,response2);
         
-      })
-      )
+        console.log(response1);
+
+      }))
       
   },[]);
+
     
   return(
       <div>
         {lists1.map(list => {
-           return (<div key={list.용산}>
-               위도: {list.위도}
+                      
+           return (
+            
+           <div key={list.용산}>
+            
+               {/* 위도: {list.위도}
                , 경도: {list.경도}
                , 주소: {list['서울특별시 용산구 설치 위치']}
-             
+                */}
+               
            </div>);
-         
+           
+
        })}
-          {lists2.map(list => {
-              return (<div key={list.안양}>
-                     위도: {list.위도}
-                  , 경도: {list.경도}
-                  , 주소: {list['소재지도로명주소']}
-                
-              </div>);
-            
-          })}
-        
-       <PageWrapper><MapContainer></MapContainer></PageWrapper>
+          
+       <PageWrapper><MapContainer/></PageWrapper>
    </div>
   );
   
 };
+
 
 export default MapPage;
--- making_cloud/src/pages/kakaoMaps.js
+++ making_cloud/src/pages/kakaoMaps.js
@@ -1,20 +1,76 @@
 import React, { useEffect } from 'react';
 
-const { kakao } = window;
+
 
 const MapContainer = () => {
+    
+    const { kakao } = window;
 
-    useEffect(() => {
+    useEffect(() => {  //지도 표시 시작
         const container = document.getElementById('myMap');
 		const options = {
-			center: new kakao.maps.LatLng(37.0113489210134, 127.26392571237953),
-			level: 3
+			center: new kakao.maps.LatLng(37.553149, 126.968881), // 지도의 중심좌표
+			level: 6
 		};
-        const map = new kakao.maps.Map(container, options);
+        const map = new kakao.maps.Map(container, options); //지도 생성
+
+        var zoomControl = new kakao.maps.ZoomControl(); //지도 줌 설정
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT); //지도 줌 설정
+        
+
+        var positions = [   // 마커를 표시할 위치
+            {
+                title: '서울특별시 용산구 서울역 광장 15번출구', 
+                latlng: new kakao.maps.LatLng(37.553149, 126.968881)
+            },
+            {
+                title: ' 서울특별시 용산구 서울역 광장 1번출구', 
+                latlng: new kakao.maps.LatLng(37.553760, 126.969662)
+            },
+            {
+                title: '서울특별시 용산구 용산역 광장', 
+                latlng: new kakao.maps.LatLng(37.528404, 126.965569)
+            },
+            {
+                title: '서울특별시 용산구 용산구청 옥상',
+                latlng: new kakao.maps.LatLng(37.532709, 126.990000)
+            },
+            {
+                title: '서울특별시 용산구 용산구청 2층 외부', 
+                latlng: new kakao.maps.LatLng(37.532709,126.990000)
+            },
+            {
+                title: '서울특별시 용산구 용산경찰서 1층 외부', 
+                latlng: new kakao.maps.LatLng(37.541169, 126.967650)
+            },
+            {
+                title: '서울특별시 용산구 원효지구대 방범순찰대(옛 용산구청)', 
+                latlng: new kakao.maps.LatLng(37.538716, 126.965890)
+            }
+            
+        ];
+
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";   //마커 이미지 지정
+        for (var i = 0; i < positions.length; i ++) {
+            var imageSize = new kakao.maps.Size(24, 35); //마커 사이즈
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); //마커 생성
+        
+            var marker = new kakao.maps.Marker({    // 마커를 생성
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title : positions[i].title,
+                 image:markerImage //이미지 못 골라서 임시 주석
+            });
+        }
+        
+
+    
     }, []);
+
+
     return (
-        <div id='myMap' style={{
-            width: '700px', 
+        <div id='myMap' style={{    //지도 크기
+            width: '790px', 
             height: '550px'
         }}></div>
     );
