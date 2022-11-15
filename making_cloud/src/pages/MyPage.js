cbee55a08a0598dc86a2f2622f94c0e07af69f5b
ysy03 <tmdduq5884@gmail.com>
2022-11-15 14:16:20 +0900

    feat: 로그인 ui 제작

--- making_cloud/src/pages/MyPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageWrapper from "../component/Wrapper/PageWrapper";
-
-function MyPage(props) {
-  return <PageWrapper>마이페이지입니다.</PageWrapper>;
-}
-
-export default MyPage;
--- making_cloud/src/pages/SettingsPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageWrapper from "../component/Wrapper/PageWrapper";
-
-function SettingsPage(props) {
-  return <PageWrapper>세팅페이지 입니다.</PageWrapper>;
-}
-
-export default SettingsPage;
